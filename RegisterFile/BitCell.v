336d9e3432bbcdbc7112308e4053f0dcf9cd8719
kkangle <liu574@wisc.edu>
2018-02-26 21:46:42 -0600

    modified decoder so it follows Karu's rules, add several other file

--- RegisterFile/BitCell.v
+++ RegisterFile/BitCell.v
@@ -1,14 +1,19 @@
 module BitCell(clk, rst, D, WriteEnable, ReadEnable1, ReadEnable2, Bitline1, Bitline2);
 
-input clk,  
-input rst, 
-input D, 
-input WriteEnable, 
-input ReadEnable1, 
-input ReadEnable2, 
-inout Bitline1, 
-inout Bitline2
+input clk;
+input rst; 
+input D;
+input WriteEnable;
+input ReadEnable1; 
+input ReadEnable2; 
+inout Bitline1;
+inout Bitline2;
 
+wire q;
 
+dff ff(.q(q), .d(D), .wen(WriteEnable), .clk(clk), .rst(rst));
+
+assign Bitline1 = ReadEnable1? q: 1'b0;
+assign Bitline2 = ReadEnable2? q: 1'b0;
 
 endmodule
--- RegisterFile/WriteDecoder_4_16.v
+++ RegisterFile/WriteDecoder_4_16.v
@@ -1,9 +1,19 @@
 module WriteDecoder_4_16(input [3:0] RegId, input WriteReg, output [15:0] Wordline);
 
-wire [15:0] temp = 16'h0001;
-
-assign tempaddr = temp << RegId;
-
-assign Wordline = (WriteReg) ? tempaddr: 16'h0000;
+assign Wordline =   (RegId == 4'b0000) ? 16'h0001 & WriteReg:
+					(RegId == 4'b0001) ? 16'h0002 & WriteReg:
+					(RegId == 4'b0010) ? 16'h0004 & WriteReg:
+					(RegId == 4'b0011) ? 16'h0008 & WriteReg:
+					(RegId == 4'b0100) ? 16'h0010 & WriteReg:
+					(RegId == 4'b0101) ? 16'h0020 & WriteReg:
+					(RegId == 4'b0110) ? 16'h0040 & WriteReg:
+					(RegId == 4'b0111) ? 16'h0080 & WriteReg:
+					(RegId == 4'b1000) ? 16'h0100 & WriteReg:
+					(RegId == 4'b1001) ? 16'h0200 & WriteReg:
+					(RegId == 4'b1010) ? 16'h0400 & WriteReg:
+					(RegId == 4'b1011) ? 16'h0800 & WriteReg:
+					(RegId == 4'b1100) ? 16'h1000 & WriteReg:
+					(RegId == 4'b1101) ? 16'h2000 & WriteReg:
+					(RegId == 4'b1110) ? 16'h4000 & WriteReg: 16'h8000 & WriteReg;	
 
 endmodule
